3494abd5fc36b6c363fd6cda5ecece93fc7cab19
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-01-12 23:45:35 +0530

    Completed the Pages for the client side the data is hard filled for now but will complete that by tomorrow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,111 @@
-import Login from './pages/Login'
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
+import Trash from "./pages/Trash";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import TaskDetails from "./pages/TaskDetails";
+import { Toaster } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { Fragment, useRef } from "react";
+import { setIsSidebarOpen } from "./redux/slices/authSlice";
+import { Transition } from "@headlessui/react";
+import clsx from "clsx";
+import { IoClose } from "react-icons/io5";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const user = true;
+  // const {user} = useSelector((state) => state.auth);
+  // console.log("User is logged in:", user);
 
-  return (
-    <Login />
-  )
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        <Sidebar />
+      </div>
+      <MobileSidebar />
+      <div className="flex-1 overflow-y-auto">
+        <Navbar />
+        <div className="p-4 2xl:px-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 }
 
-export default App
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setIsSidebarOpen(false));
+  };
+
+  return (
+    <Transition
+      show={isSidebarOpen}
+      as={Fragment}
+      enter="transition-opacity duration-700 ease-out"
+      enterFrom="opacity-0 -translate-x-full"
+      enterTo="opacity-100 translate-x-0"
+      leave="transition-opacity duration-700 ease-in-out"
+      leaveFrom="opacity-100 translate-x-0"
+      leaveTo="opacity-0 -translate-x-full"
+    >
+      <div
+        ref={(node) => (mobileMenuRef.current = node)}
+        className="fixed inset-0 z-50 md:hidden bg-black/40"
+      >
+        <div
+          className="relative bg-white w-3/4 h-screen transform transition-transform duration-300 ease-in-out"
+          onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the menu
+        >
+          <div className="w-full flex justify-end px-5 py-0">
+            <button
+              onClick={() => closeSidebar()}
+              className="absolute flex justify-end items-center mt-6"
+            >
+              <IoClose size={25} />
+            </button>
+          </div>
+          <div>
+            <Sidebar />
+          </div>
+        </div>
+      </div>
+    </Transition>
+  );
+};
+
+function App() {
+  return (
+    <main className="w-full min-h-screen bg-[#f3f4f6] ">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Toaster richColors />
+    </main>
+  );
+}
+
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
+import { useSelector } from "react-redux";
 
 const Login = () => {
-  return <div>Login</div>;
+  const {user} = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user, navigate]);
+
+  const submitHandler = async () => {
+    console.log("Submit");
+  };
+
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+        {/* Left Side */}
+        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
+              Manage all your work at one place!
+            </span>
+            <p className="flex gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
+              <span>Task</span>
+              <span>Flow</span>
+            </p>
+
+            <div>
+              <div className="circle rotate-in-up-left"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className="form-container w-full md:w-[450px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
+          >
+            <div className="">
+              <p className="text-blue-600 text-3xl font-bold text-center">
+                Hey! How you doin'?
+              </p>
+              <p className="text-center text-gray-700 text-base">
+                Keep all your information safe.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-y-5">
+              <Textbox
+                type="email"
+                name="email"
+                label="Email Address"
+                placeholder="Email Please"
+                register={register("email", {
+                  required: "Email is required",
+                })}
+                error={errors.email ? errors.email.message : ""}
+                className="w-full rounded-full"
+              />
+              <Textbox
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Password Please"
+                register={register("password", {
+                  required: "Password is required",
+                })}
+                error={errors.password ? errors.password.message : ""}
+                className="w-full rounded-full"
+              />
+
+              <span className="text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer transition-all">
+                Forget Password?
+              </span>
+
+              <Button
+                type="submit"
+                label="Login"
+                className="w-full rounded-full bg-blue-600 h-10 text-white transition-all hover:bg-blue-700"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
